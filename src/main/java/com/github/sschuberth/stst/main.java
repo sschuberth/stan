1943ea25eb4220a129a7cd67cfc0e344f132760e
Sebastian Schuberth <sschuberth@gmail.com>
2017-01-01 14:49:12 +0100

    Add initial test resources
    And pass the file to convert as a command line argument instead of
    hard-coding the path.

--- src/main/java/com/github/sschuberth/stst/main.java
+++ src/main/java/com/github/sschuberth/stst/main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 class Main {
     public static void main(String[] args) {
-        String filename = "C:\\Development\\Projects\\GitHub\\StatementStats\\src\\test\\resources\\PB_KAZ_KtoNr_0680330308_06-01-2016_0621.pdf";
+        String filename = args[0];
         List<BookingItem> items = null;
         try {
             items = PostbankPDFParser.parse(filename);
---
+++ src/test/resources/README.md
@@ -0,0 +1,3 @@
+# Original links to test files
+
+[317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313.pdf](https://www.scribd.com/document/317970916/PB-KAZ-KtoNr-0914083113-03-06-2016-0313)
