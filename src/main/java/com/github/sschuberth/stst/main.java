b9bdc10a958d29e8b816c1ede8d954045d18621e
Sebastian Schuberth <sschuberth@gmail.com>
2017-01-18 21:36:18 +0100

    main: Take wildcard arguments and parse into a list of statements

--- src/main/java/com/github/sschuberth/stst/main.java
+++ src/main/java/com/github/sschuberth/stst/main.java
@@ -1,19 +1,36 @@
 package com.github.sschuberth.stst;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.file.Files.newDirectoryStream;
 
 class Main {
     public static void main(String[] args) {
-        String filename = args[0];
-        List<BookingItem> items = null;
-        try {
-            items = PostbankPDFParser.parse(filename);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        for (BookingItem item : items) {
-            System.out.println(item);
+        List<Statement> statements = new ArrayList<>();
+
+        for (String arg : args) {
+            File file = new File(arg);
+
+            try (DirectoryStream<Path> stream = newDirectoryStream(file.getParentFile().toPath(), file.getName())) {
+                stream.forEach(filename -> {
+                    try {
+                        Statement st = PostbankPDFParser.parse(filename.toString());
+                        System.out.println("Successfully parsed statement '" + file.getName() + "' dated from " + st.fromDate + " to " + st.toDate + ".");
+                        statements.add(st);
+                    } catch (ParseException e) {
+                        System.err.println("Error parsing '" + filename + "'.");
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                System.err.println("Error opening '" + arg + "'.");
+            }
         }
     }
 }
