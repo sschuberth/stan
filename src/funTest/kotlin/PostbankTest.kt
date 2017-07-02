d2543efe36dd2a50592577ec6413a2e4a31f45fc
Sebastian Schuberth <sschuberth@gmail.com>
2017-07-02 17:30:00 +0200

    Add functional tests written in Kotlin

---
+++ src/funTest/kotlin/PostbankTest.kt
@@ -0,0 +1,34 @@
+package com.github.sschuberth.stan.functionaltest
+
+import com.github.salomonbrys.kotson.fromJson
+import com.github.sschuberth.stan.parsers.PostbankPDFParser
+
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class PostbankTest : StringSpec({
+    val gson = Gson()
+
+    "Demokonto account statement is parsed correctly" {
+        val expectedText = readResource("/PB_KAZ_KtoNr_9999999999_06-04-2017_1200-expected.json")
+        val expectedJson = gson.fromJson<JsonArray>(expectedText)
+
+        val actualText = PostbankPDFParser.parse("PB_KAZ_KtoNr_9999999999_06-04-2017_1200.pdf").toString()
+        val actualJson = gson.fromJson<JsonArray>(actualText)
+
+        actualJson shouldBe expectedJson
+    }
+
+    "Goran Bolsec account statement is parsed correctly" {
+        val expectedText = readResource("/317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313-expected.json")
+        val expectedJson = gson.fromJson<JsonArray>(expectedText)
+
+        val actualText = PostbankPDFParser.parse("317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313.pdf").toString()
+        val actualJson = gson.fromJson<JsonArray>(actualText)
+
+        actualJson shouldBe expectedJson
+    }
+})
