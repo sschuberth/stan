105d3c3d4e59f9d9745ee7d6f26b92ac90dada3f
Sebastian Schuberth <sschuberth@gmail.com>
2017-11-12 15:51:44 +0100

    OfxV1Exporter: Remove an unused import

--- src/main/java/com/github/sschuberth/stan/exporters/OfxV1Exporter.java
+++ src/main/java/com/github/sschuberth/stan/exporters/OfxV1Exporter.java
@@ -12,7 +12,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Currency;
-import java.util.Locale;
 
 public enum OfxV1Exporter implements Exporter {
     OFX,
