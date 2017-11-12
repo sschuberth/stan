44b0ce53ab3501c4affc201e8fef1c047aead553
Sebastian Schuberth <sschuberth@gmail.com>
2017-11-12 21:56:34 +0100

    Add functional tests for the OFX v1 exporter

---
+++ src/funTest/kotlin/OfxV1ExporterTest.kt
@@ -0,0 +1,41 @@
+package com.github.sschuberth.stan.functionaltest
+
+import com.github.sschuberth.stan.exporters.OfxV1Exporter
+import com.github.sschuberth.stan.parsers.PostbankPDFParser
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+import java.io.File
+
+class OfxV1ExporterTest : StringSpec({
+    "Demokonto account statement is exported correctly" {
+        val baseName = "PB_KAZ_KtoNr_9999999999_06-04-2017_1200"
+
+        val expectedOfx = readResource("/$baseName-expected.ofx")
+                .replace(Regex("(<DTSERVER>)\\d+"), "\\1")
+
+        val ofx = File.createTempFile(baseName, "ofx")
+        val statement = PostbankPDFParser.parse("$baseName.pdf")
+        OfxV1Exporter.OFX.write(statement, ofx.path)
+        val actualOfx = ofx.readText().replace(Regex("(<DTSERVER>)\\d+"), "\\1")
+
+        ofx.delete() shouldBe true
+        actualOfx shouldBe expectedOfx
+    }
+
+    "Goran Bolsec account statement is exported correctly" {
+        val baseName = "317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313"
+
+        val expectedOfx = readResource("/$baseName-expected.ofx")
+                .replace(Regex("(<DTSERVER>)\\d+"), "\\1")
+
+        val ofx = File.createTempFile(baseName, "ofx")
+        val statement = PostbankPDFParser.parse("$baseName.pdf")
+        OfxV1Exporter.OFX.write(statement, ofx.path)
+        val actualOfx = ofx.readText().replace(Regex("(<DTSERVER>)\\d+"), "\\1")
+
+        ofx.delete() shouldBe true
+        actualOfx shouldBe expectedOfx
+    }
+})
