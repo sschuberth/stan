dfd7a9a06bdc00579db00347bd9e15b446925b0f
Sebastian Schuberth <sschuberth@gmail.com>
2017-11-14 13:36:23 +0100

    JsonExporter: Ensure to write output with UTF-8 encoding

--- src/main/kotlin/exporters/JsonExporter.kt
+++ src/main/kotlin/exporters/JsonExporter.kt
@@ -1,11 +1,9 @@
 package com.github.sschuberth.stan.exporters
 
 import com.github.sschuberth.stan.model.Statement
-
-import java.io.PrintWriter
+import java.io.File
 
 class JsonExporter : Exporter {
-    override fun write(statement: Statement, filename: String) {
-        PrintWriter(filename).use { out -> out.print(statement.toString()) }
-    }
+    override fun write(statement: Statement, filename: String) =
+        File(filename).writeText(statement.toString())
 }
