e579d195a96cce2c9b96041a231bed4b0f0afe86
Sebastian Schuberth <sschuberth@gmail.com>
2017-11-14 12:05:14 +0100

    Replace PostbankTest with JsonExporterTest
    This way all current exporters are covered.

--- src/funTest/kotlin/JsonExporterTest.kt
+++ src/funTest/kotlin/JsonExporterTest.kt
@@ -1,6 +1,7 @@
 package com.github.sschuberth.stan.functionaltest
 
 import com.github.salomonbrys.kotson.fromJson
+import com.github.sschuberth.stan.exporters.JsonExporter
 import com.github.sschuberth.stan.parsers.PostbankPDFParser
 
 import com.google.gson.GsonBuilder
@@ -9,28 +10,38 @@ import com.google.gson.JsonArray
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class PostbankTest : StringSpec({
+import java.io.File
+
+class JsonExporterTest : StringSpec({
     val gson = GsonBuilder().setPrettyPrinting().create()
 
-    "Demokonto account statement is parsed correctly" {
+    "Demokonto account statement is exported correctly" {
         val baseName = "PB_KAZ_KtoNr_9999999999_06-04-2017_1200"
 
         val expectedText = readResource("/$baseName-expected.json")
         val expectedJson = gson.toJson(gson.fromJson<JsonArray>(expectedText))
 
-        val actualJson = PostbankPDFParser.parse("$baseName.pdf").toString()
+        val json = File.createTempFile(baseName, "json")
+        val statement = PostbankPDFParser.parse("$baseName.pdf")
+        JsonExporter().write(statement, json.path)
+        val actualJson = json.readText()
 
+        json.delete() shouldBe true
         actualJson shouldBe expectedJson
     }
 
-    "Goran Bolsec account statement is parsed correctly" {
+    "Goran Bolsec account statement is exported correctly" {
         val baseName = "317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313"
 
         val expectedText = readResource("/$baseName-expected.json")
         val expectedJson = gson.toJson(gson.fromJson<JsonArray>(expectedText))
 
-        val actualJson = PostbankPDFParser.parse("$baseName.pdf").toString()
+        val json = File.createTempFile(baseName, "json")
+        val statement = PostbankPDFParser.parse("$baseName.pdf")
+        JsonExporter().write(statement, json.path)
+        val actualJson = json.readText()
 
+        json.delete() shouldBe true
         actualJson shouldBe expectedJson
     }
 })
