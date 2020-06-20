4cf6c7a15dd849cc7ac391094f10f90fb644ba25
Sebastian Schuberth <sschuberth@gmail.com>
2020-06-20 16:46:41 +0200

    lib: Get rid of the dependency on ImplicitReflectionSerializer

--- lib/src/funTest/kotlin/JsonExporterTest.kt
+++ lib/src/funTest/kotlin/JsonExporterTest.kt
@@ -11,16 +11,14 @@ import io.kotest.matchers.shouldBe
 import java.io.File
 import java.io.FileOutputStream
 
-import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.stringify
+import kotlinx.serialization.json.JsonElement
 
-@ImplicitReflectionSerializer
 class JsonExporterTest : StringSpec({
     "Demokonto account statement is exported correctly" {
         val baseName = "PB_KAZ_KtoNr_9999999999_06-04-2017_1200"
 
         val expectedText = File("src/funTest/assets/$baseName-expected.json").readText()
-        val expectedJson = JSON.stringify(JSON.parseJson(expectedText))
+        val expectedJson = JSON.stringify(JsonElement.serializer(), JSON.parseJson(expectedText))
 
         val jsonFile = createTempFile(suffix = ".json")
         val statement = PostbankPDFParser.parse(File("src/funTest/assets/$baseName.pdf"))
@@ -36,7 +34,7 @@ class JsonExporterTest : StringSpec({
         val baseName = "317970916-PB-KAZ-KtoNr-0914083113-03-06-2016-0313"
 
         val expectedText = File("src/funTest/assets/$baseName-expected.json").readText()
-        val expectedJson = JSON.stringify(JSON.parseJson(expectedText))
+        val expectedJson = JSON.stringify(JsonElement.serializer(), JSON.parseJson(expectedText))
 
         val jsonFile = createTempFile(suffix = ".json")
         val statement = PostbankPDFParser.parse(File("src/funTest/assets/$baseName.pdf"))
--- lib/src/main/kotlin/exporters/JsonExporter.kt
+++ lib/src/main/kotlin/exporters/JsonExporter.kt
@@ -4,10 +4,8 @@ import dev.schuberth.stan.model.Statement
 
 import java.io.OutputStream
 
-import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
-import kotlinx.serialization.stringify
 
 val JSON = Json(JsonConfiguration.Stable.copy(prettyPrint = true, indent = "  "))
 
@@ -17,7 +15,6 @@ val JSON = Json(JsonConfiguration.Stable.copy(prettyPrint = true, indent = "  ")
 class JsonExporter : Exporter {
     override val extension = "json"
 
-    @ImplicitReflectionSerializer
     override fun write(statement: Statement, output: OutputStream) =
-        output.use { it.write(JSON.stringify(statement).toByteArray()) }
+        output.use { it.write(JSON.stringify(Statement.serializer(), statement).toByteArray()) }
 }
