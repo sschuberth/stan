7ebaf7ebb0e7a89145673b2538661ca1419d7c49
Sebastian Schuberth <sschuberth@gmail.com>
2021-03-13 12:43:23 +0100

    Main: Show default values in help

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ package dev.schuberth.stan.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.core.UsageError
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.convert
@@ -127,6 +129,12 @@ class Stan : CliktCommand() {
     private val statementGlobs by argument()
         .file(mustExist = false, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = false)
         .multiple()
+
+    init {
+        context {
+            helpFormatter = CliktHelpFormatter(showDefaultValues = true)
+        }
+    }
 
     override fun run() {
         if (statementGlobs.isEmpty()) throw UsageError("No statement file(s) specified.")
