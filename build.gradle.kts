83794f26d5bf4aa9a4148f95c75b787000b558b6
Sebastian Schuberth <sschuberth@gmail.com>
2021-12-12 18:42:32 +0100

    Gradle: Use extensions to configure source sets
    The concept of conventions is deprecated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val detektPluginVersion: String by project
@@ -59,7 +58,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     sourceSets.create("funTest") {
-        withConvention(KotlinSourceSet::class) {
+        kotlin.sourceSets.getByName(name) {
             kotlin.srcDirs("src/funTest/kotlin")
             resources.srcDirs("src/funTest/resources")
         }
