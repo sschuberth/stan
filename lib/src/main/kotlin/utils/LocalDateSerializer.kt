30d62cf1224921b5f6962ad226052d8f9861c585
Sebastian Schuberth <sschuberth@gmail.com>
2022-01-04 18:10:43 +0100

    Simplify the code for serializers a bit

--- lib/src/main/kotlin/utils/LocalDateSerializer.kt
+++ lib/src/main/kotlin/utils/LocalDateSerializer.kt
@@ -4,17 +4,11 @@ import java.time.LocalDate
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializer(forClass = LocalDate::class)
+@Serializer(LocalDate::class)
 object LocalDateSerializer : KSerializer<LocalDate> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("WithCustomDefault", PrimitiveKind.STRING)
-
     override fun serialize(encoder: Encoder, value: LocalDate) = encoder.encodeString(value.toString())
-
-    override fun deserialize(decoder: Decoder) = LocalDate.parse(decoder.decodeString())
+    override fun deserialize(decoder: Decoder): LocalDate = LocalDate.parse(decoder.decodeString())
 }
--- lib/src/main/kotlin/utils/LocaleSerializer.kt
+++ lib/src/main/kotlin/utils/LocaleSerializer.kt
@@ -4,17 +4,11 @@ import java.util.Locale
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializer(forClass = Locale::class)
+@Serializer(Locale::class)
 object LocaleSerializer : KSerializer<Locale> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("WithCustomDefault", PrimitiveKind.STRING)
-
     override fun serialize(encoder: Encoder, value: Locale) = encoder.encodeString(value.toString())
-
-    override fun deserialize(decoder: Decoder) = Locale(decoder.decodeString())
+    override fun deserialize(decoder: Decoder): Locale = Locale(decoder.decodeString())
 }
