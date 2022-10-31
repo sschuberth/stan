151d00c4a48eedde74eb79df3259ec99e8488cb0
Sebastian Schuberth <sschuberth@gmail.com>
2022-10-31 12:50:29 +0100

    ProjectConfig: Remove the `isRunningFromIdea` heuristic
    This does not work reliably across IDEA versions.

--- lib/src/funTest/kotlin/ProjectConfig.kt
+++ lib/src/funTest/kotlin/ProjectConfig.kt
@@ -16,8 +16,5 @@ class ProjectConfig : AbstractProjectConfig() {
         startKoin {
             modules(configModule)
         }
-
-        val isRunningFromIdea = "idea_rt.jar" in System.getProperty("java.class.path")
-        if (isRunningFromIdea) System.setProperty("kotest.assertions.multi-line-diff", "simple")
     }
 }
