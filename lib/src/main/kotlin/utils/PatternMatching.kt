777151f85a95ea854f0eb07145e677d32928dec6
Sebastian Schuberth <sschuberth@gmail.com>
2022-11-15 15:33:43 +0100

    Add a simple implementation of pattern matching
    Currently, only Regex objects can be matched.

---
+++ lib/src/main/kotlin/utils/PatternMatching.kt
@@ -0,0 +1,11 @@
+package dev.schuberth.stan.utils
+
+class PatternMatching(private val value: String) {
+    fun pattern(pattern: Regex, block: MatchResult.() -> Unit) {
+        pattern.matchEntire(value)?.block()
+    }
+}
+
+fun whenMatch(value: String, block: PatternMatching.() -> Unit) {
+    PatternMatching(value).block()
+}
