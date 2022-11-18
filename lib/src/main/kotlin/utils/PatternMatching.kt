68b02c2c1108442c7a326076e86e98f61da665a2
Sebastian Schuberth <sschuberth@gmail.com>
2022-11-18 14:44:05 +0100

    PatternMatching: Turn short functions into expressions

--- lib/src/main/kotlin/utils/PatternMatching.kt
+++ lib/src/main/kotlin/utils/PatternMatching.kt
@@ -1,11 +1,7 @@
 package dev.schuberth.stan.utils
 
 class PatternMatching(private val value: String) {
-    fun pattern(pattern: Regex, block: MatchResult.() -> Unit) {
-        pattern.matchEntire(value)?.block()
-    }
+    fun pattern(pattern: Regex, block: MatchResult.() -> Unit) = pattern.matchEntire(value)?.block()
 }
 
-fun whenMatch(value: String, block: PatternMatching.() -> Unit) {
-    PatternMatching(value).block()
-}
+fun whenMatch(value: String, block: PatternMatching.() -> Unit) = PatternMatching(value).block()
