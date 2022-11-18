0f8aa1eaf226255a5272fe3308739c759fe05a37
Sebastian Schuberth <sschuberth@gmail.com>
2022-11-18 14:54:10 +0100

    PatternMatching: Add functions for prefix / suffix matching

--- lib/src/main/kotlin/utils/PatternMatching.kt
+++ lib/src/main/kotlin/utils/PatternMatching.kt
@@ -2,6 +2,12 @@ package dev.schuberth.stan.utils
 
 class PatternMatching(private val value: String) {
     fun pattern(pattern: Regex, block: MatchResult.() -> Unit) = pattern.matchEntire(value)?.block()
+
+    fun pattern(prefix: String, block: String.() -> Unit) = value.withoutPrefix(prefix)?.block()
 }
 
 fun whenMatch(value: String, block: PatternMatching.() -> Unit) = PatternMatching(value).block()
+
+fun String.withoutPrefix(prefix: String) = removePrefix(prefix).takeIf { it != this }
+
+fun String.withoutSuffix(suffix: String) = removeSuffix(suffix).takeIf { it != this }
