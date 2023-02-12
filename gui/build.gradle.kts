f3fc33ba098a3fd9f2688d6b5cef1c85ec35dc97
Sebastian Schuberth <sschuberth@gmail.com>
2023-02-12 11:20:58 +0100

    Gradle: Upgrade the Kotlin plugin to version 1.8.10
    See [1]. This also requires to upgrade the Compose compiler, see [2].
    
    [1]: https://github.com/JetBrains/kotlin/releases/tag/v1.8.10
    [2]: https://androidx.dev/storage/compose-compiler/repository

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 compose {
     // See https://androidx.dev/storage/compose-compiler/repository
     // and https://github.com/JetBrains/compose-jb/blob/master/VERSIONING.md#using-jetpack-compose-compiler.
-    kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.0-dev-k1.8.0-33c0ad36f83")
+    kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.1-dev-k1.8.10-c312d77f4cb")
 }
 
 dependencies {
