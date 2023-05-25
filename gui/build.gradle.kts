8cbea96914dd22c5141a9f2fe64acf6a6db48aa6
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-25 13:04:58 +0200

    Gradle: Upgrade Compose to version 1.4.0

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -2,28 +2,12 @@ plugins {
     alias(libs.plugins.compose)
 }
 
-repositories {
-    exclusiveContent {
-        forRepository {
-            maven("https://androidx.dev/storage/compose-compiler/repository")
-        }
-
-        filter {
-            includeGroup("androidx.compose.compiler")
-        }
-    }
-}
-
 compose {
     desktop {
         application {
             mainClass = "dev.schuberth.stan.gui.MainKt"
         }
     }
-
-    // See https://androidx.dev/storage/compose-compiler/repository
-    // and https://github.com/JetBrains/compose-jb/blob/master/VERSIONING.md#using-jetpack-compose-compiler.
-    kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.1-dev-k1.8.10-c312d77f4cb")
 }
 
 dependencies {
