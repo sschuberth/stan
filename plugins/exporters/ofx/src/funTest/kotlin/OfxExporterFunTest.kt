156d7a665537a666fded97d263f392da92aac346
Sebastian Schuberth <sschuberth@gmail.com>
2023-06-09 13:08:45 +0200

    Ofx: Fix replacements in tests

--- plugins/exporters/ofx/src/funTest/kotlin/OfxExporterFunTest.kt
+++ plugins/exporters/ofx/src/funTest/kotlin/OfxExporterFunTest.kt
@@ -2,7 +2,9 @@ package dev.schuberth.stan.plugins.exporters.ofx
 
 import dev.schuberth.stan.AbstractTextExporterFunTest
 
+import java.time.LocalDateTime
+
 class OfxExporterFunTest : AbstractTextExporterFunTest(
     OfxExporter(),
-    { replace(Regex("(<DTSERVER>)\\d+"), "\\1") }
+    { replace(Regex("(<DTSERVER>)\\d+"), "$1${LocalDateTime.now().format(OfxExporter.DATE_FORMATTER)}") }
 )
