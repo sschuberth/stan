bd1fc784f99110baa1294534025cbe05c67402d6
Sebastian Schuberth <sschuberth@gmail.com>
2023-07-13 14:29:20 +0200

    Upgrade clikt to version 4.0.0
    See [1].
    
    [1]: https://github.com/ajalt/clikt/blob/master/CHANGELOG.md#400

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.core.UsageError
 import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.output.CliktHelpFormatter
+import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.convert
@@ -62,7 +62,7 @@ class Main : CliktCommand(invokeWithoutSubcommand = true) {
 
     init {
         context {
-            helpFormatter = CliktHelpFormatter(requiredOptionMarker = "*", showDefaultValues = true)
+            helpFormatter = { MordantHelpFormatter(context = it, requiredOptionMarker = "*", showDefaultValues = true) }
         }
 
         subcommands(ExportCommand(), FilterCommand())
