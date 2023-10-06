501a2f3484983b32dcd699975ea2a49ad817ef59
Sebastian Schuberth <sschuberth@gmail.com>
2023-10-06 10:52:16 +0200

    docs(Logger): Improve logger configuration docs

--- lib/src/main/kotlin/utils/Logger.kt
+++ lib/src/main/kotlin/utils/Logger.kt
@@ -4,7 +4,9 @@ import java.lang.System.Logger.Level
 import java.util.concurrent.ConcurrentHashMap
 
 private val loggerCache = ConcurrentHashMap<String, System.Logger>().also {
-    // See: https://docs.oracle.com/javase/7/docs/api/java/util/logging/SimpleFormatter.html#format(java.util.logging.LogRecord)
+    // The System.Logger API by default uses the java.util.logging.Logger implementation, so formatting needs to be
+    // customized via java.util.logging.* properties, see
+    // https://docs.oracle.com/cd/E57471_01/bigData.100/data_processing_bdd/src/rdp_logging_config.html
     System.setProperty("java.util.logging.SimpleFormatter.format", "%1\$tF %1\$tT.%1\$tL %4$-7s %3\$s - %5\$s%n")
 }
 
