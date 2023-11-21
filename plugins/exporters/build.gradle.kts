e6fd7b458ed9bddd6eec7a969d683e6318b606f0
Sebastian Schuberth <sschuberth@gmail.com>
2023-11-21 21:43:32 +0100

    build(Gradle): Simplify the code to declare plugin platforms
    Avoid repeating code from `settings.gradle.kts`.

--- plugins/exporters/build.gradle.kts
+++ plugins/exporters/build.gradle.kts
@@ -7,11 +7,7 @@ javaPlatform {
 }
 
 dependencies {
-    projectDir.walk().maxDepth(2).filter {
-        it.parentFile != projectDir && it.isFile && it.name == "build.gradle.kts"
-    }.mapTo(mutableListOf()) {
-        it.parentFile.toRelativeString(rootDir).replace(File.separatorChar, ':')
-    }.forEach {
-        api(project(":$it-${projectDir.name.removeSuffix("s")}"))
+    project.subprojects.forEach {
+        api(it)
     }
 }
